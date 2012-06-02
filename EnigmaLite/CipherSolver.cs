c81cfd0afff6d57ca90af84fac0e8444c0729029
Chris Bowdon <c.bowdon@gmail.com>
2012-06-02 20:49:01 +0100

    Reshaping the model into CipherSolver class

---
+++ EnigmaLite/CipherSolver.cs
@@ -0,0 +1,11 @@
+using System;
+namespace EnigmaLite
+{
+	public class CipherSolver
+	{
+		public CipherSolver ()
+		{
+		}
+	}
+}
+
---
+++ EnigmaLiteTests/CipherTests.cs
@@ -0,0 +1,11 @@
+using System;
+namespace EnigmaLiteTests
+{
+	public class CipherTests
+	{
+		public CipherTests ()
+		{
+		}
+	}
+}
+
