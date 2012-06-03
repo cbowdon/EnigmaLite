18d34213063f1de869f3bfede206b4aba9d50885
Chris Bowdon <c.bowdon@gmail.com>
2012-06-03 09:30:06 +0100

    Created CipherDictionary; CipherSolver passes tests

---
+++ EnigmaLiteTests/SerializationTests.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace EnigmaLiteTests
+{
+	[TestFixture()]
+	public class SerializationTests
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
