45e65a1500f6b0d310931adb2e35c8a7f5086b8a
Chris Bowdon <c.bowdon@gmail.com>
2012-06-03 06:51:55 +0100

    Added CipherSolver tests

--- EnigmaLite/CipherSolver.cs
+++ EnigmaLite/CipherSolver.cs
@@ -1,10 +1,19 @@
 using System;
+using System.Collections.Generic;
+
 namespace EnigmaLite
 {
 	public class CipherSolver
 	{
-		public CipherSolver ()
+		public string Problem { get; private set; }
+		public string Solution { get; private set; }
+		public Dictionary<char,char> Cipher { get; set; }
+		public double SolutionScore { get; private set; }
+		
+		public CipherSolver (string encryptedText)
 		{
+			Problem = encryptedText;
+			
 		}
 	}
 }
--- EnigmaLiteTests/CipherTests.cs
+++ EnigmaLiteTests/CipherTests.cs
@@ -1,10 +1,49 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
+using NUnit.Framework;
+using EnigmaLite;
+
 namespace EnigmaLiteTests
 {
 	public class CipherTests
 	{
-		public CipherTests ()
+		protected string shortStory = "DNA - Private Life Of Genghis Khan.txt";		
+		protected string cleanText;
+		protected Dictionary<char,char> cipher;
+		protected string crypted;
+		
+		public CipherTests()
 		{
+			// load some text
+            cleanText = File.ReadAllText (shortStory);			
+			
+			// create basic substition cipher			
+			cipher = new Dictionary<char, char> ();
+			for (int i = 0; i < 255; i++) {
+				cipher.Add ((char)i, (char)(i + 1));
+			}
+			cipher.Add ((char)255, (char)0); 
+			
+			crypted = cleanText.SubChars(cipher);			
+		}
+		
+		
+		[Test]
+		public void CipherSolver ()
+		{
+			// instantiating class should give first solution
+			CipherSolver solver = new CipherSolver(crypted);			
+			Assert.AreEqual(cleanText, solver.Solution);
+			Assert.AreEqual(1.0, solver.SolutionScore, 1e-5);
+			// should be able to mess with it
+			solver.Cipher['e'] = '!';
+			Assert.AreNotEqual(cleanText, solver.Solution);
+			Assert.Less(1.0, solver.SolutionScore);
+			// and fix it
+			solver.Cipher['e'] = 'f';
+			Assert.AreEqual(cleanText, solver.Solution);
+			Assert.AreEqual(1.0, solver.SolutionScore, 1e-5);
 		}
 	}
 }
