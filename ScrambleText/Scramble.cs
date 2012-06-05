cbcab9d17a6d603e971bca2fc77b31a1cb2d0677
Chris Bowdon <c.bowdon@gmail.com>
2012-06-05 11:39:44 +0100

    Added ScrambleText utility app

---
+++ ScrambleText/Scramble.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnigmaLite;
+
+namespace ScrambleText
+{
+    class Scramble
+    {
+        static void Main(string[] args)
+        {
+            var inFilename = args.Length > 0 ? args[0] : "DNA - Private Life Of Genghis Khan.txt";
+            Console.WriteLine("Scrambling text in {0}...", inFilename);
+            var inputText = File.ReadAllText(inFilename);            
+            var gs = GenerateScramble(128);
+            var outputText = inputText.SubChars(gs);
+            var outFilename = string.Format("Scrambled {0}", inFilename);
+            using (TextWriter t = new StreamWriter(outFilename))
+            {
+                t.Write(outputText);
+            }
+            Console.WriteLine("Output file: {0}", outFilename);
+        }
+
+        static Dictionary<char,char> GenerateScramble (int num)
+        {            
+            var gs = new Dictionary<char, char>();
+            var r = new Random();
+            var offset = r.Next(num+1);
+            Func<int, int> getSub = x =>
+            {
+                var y = x + offset;
+                return y > num ? y - num : y;                
+            };
+
+            for (int i = 0; i < num+1; i++)
+            {
+                gs.Add((char)i, (char)getSub(i));                
+            }            
+            return gs;
+        }
+    }
+}
---
+++ SerializeFrequencies/SerializeFreqs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using EnigmaLite;
+
+namespace SerializeFrequencies
+{
+    class SerializeFreqs
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serializing...");
+            var inputFile = args.Length > 0 ? args[0] : "DNA - Private Life Of Genghis Khan.txt";
+			SerializeWordsAndChars (inputFile);
+            Console.WriteLine("Serialized!");			
+        }        
+		
+		static void SerializeWordsAndChars (string inputFile)
+		{
+            string cleanText;
+            List<KeyValuePair<char, double>> charFreqs;
+            List<KeyValuePair<string, double>> wordFreqs;
+          
+            cleanText = File.ReadAllText(inputFile);
+
+            wordFreqs = cleanText.SplitByWords().RankFrequency();
+            charFreqs = cleanText.SplitByChars().RankFrequency();            
+
+			using (Stream stream = File.Open("chars.bin", FileMode.Create)) {
+				BinaryFormatter bin = new BinaryFormatter ();
+				bin.Serialize (stream, charFreqs);
+			}
+			
+			using (Stream stream = File.Open("words.bin", FileMode.Create)) {
+				BinaryFormatter bin = new BinaryFormatter ();
+				bin.Serialize (stream, wordFreqs);
+			}
+		}
+    }
+}
+
